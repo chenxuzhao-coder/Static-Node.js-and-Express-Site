ab15fe727a46760627ab2e3276b1007e2788abfa
chenxuzhao-coder <62107324+chenxuzhao-coder@users.noreply.github.com>
2021-04-02 21:33:00 +0800

    2

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const { projects } = require('./data.json')
+const app = express()
+
+//use pug middleware
+app.set('view engine', 'pug')
+
+//set static file
+app.use('/static', express.static('public'))
+
+//index route
+app.get('/', (req, res) => {
+    res.render('index', {
+        Projects: projects
+    })
+})
+
+//personal info route
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+//project route
+app.get('/projects/:id', (req, res) => {
+    res.render('project', {
+        Project_name: projects[req.params.id].project_name,
+        Description: projects[req.params.id].description,
+        technologies: projects[req.params.id].technologies,
+        Live: projects[req.params.id].live_link,
+        GitHub: projects[req.params.id].github_link,
+        ProjectImgs: projects[req.params.id].image_urls.slice(1)
+    })
+})
+
+
+app.use((req, res, next) => {
+    const err = new Error('Page Not Found')
+    err.status = 404
+    next(err)
+})
+
+app.use((err, req, res, next) => {
+    if (err.status === 404) {
+        res.status(err.status)
+        res.render('error', { err })
+    } else {
+        err.message = err.message
+        res.status(err.status)
+        res.render('error', { err })
+    }
+})
+
+
+
+//set server
+app.listen(3000, () => {
+    console.log('The app is running on localhost:3000')
+})
